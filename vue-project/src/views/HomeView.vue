49310eafecab083242bc3156a2d5ae1a5dfd1601
s0579111 <s0579111@htw-berlin.de>
2024-05-05 23:56:34 +0200

    Name

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref<any[]>([])
+const name = ref<string>('')
+
+const input_content = ref<string>('')
+const input_category = ref<string | null>(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt
+}))
+
+watch(name, (newVal: string) => {
+    localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+    todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<main class="app">
+    
+    <section class="greeting">
+        <h2 class="title">
+            Hallo, <input type="text" id="name" placeholder="Name here" v-model="name">
+        </h2>
+    </section>
+
+</main>
 </template>
